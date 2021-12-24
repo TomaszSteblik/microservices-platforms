5841026f0fcfa89341004659c36a84f1d19d00cb
TomaszSteblik <tsteblik@gmail.com>
2021-12-25 00:45:51 +0100

    added env variables for api

--- AngularFrontend/src/environments/environment.prod.ts
+++ AngularFrontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  commandService: 'http://braght.com:80/api',
+  platformService: 'http://braght.com:80/api'
 };
--- AngularFrontend/src/environments/environment.ts
+++ AngularFrontend/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  commandService: 'http://localhost:6000/api',
+  platformService: 'http://localhost:5000/api'
 };
 
 /*
