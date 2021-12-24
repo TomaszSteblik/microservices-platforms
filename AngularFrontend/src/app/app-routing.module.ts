6afb6ab2c85afb31ee6772419c57ce7a3e3feb98
TomaszSteblik <tsteblik@gmail.com>
2021-12-25 00:18:56 +0100

    added 404 handling to frontend

--- AngularFrontend/src/app/app-routing.module.ts
+++ AngularFrontend/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommandComponent } from './command/command.component';
 import { HomeComponent } from './home/home.component';
 import { PlatformComponent} from './platform/platform.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'commands', component: CommandComponent },
   { path: 'platforms', component: PlatformComponent },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '404', component: NotFoundComponent},
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
--- AngularFrontend/src/app/app.module.ts
+++ AngularFrontend/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MaterialModule} from '../material.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlatformComponent,
     CommandComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
